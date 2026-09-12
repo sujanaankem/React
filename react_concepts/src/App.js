b8c301715bf44b0e5ea0fe906dd32151e257b946
sujanaankem <ankemsujana@gmail.com>
2026-09-12 18:00:16 +0530

    App

--- react_concepts/src/App.js
+++ react_concepts/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RegisterPage from './Pages/RegisterPage';
 import Login from './Pages/Login';
+import Restaurant from './Pages/Restaurant';    
 function App() {
   return (
 
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<RegisterPage /> } />
         <Route path="/login" element={<Login />}/>
+        <Route path='/Restaurant' element={<Restaurant />}/>
 
       </Routes>
     </BrowserRouter>
